70b3af6a8c14eafc6e5dcbd42074c0f160098d57
trungtruc2357 <trungtruc2357@gmail.com>
2022-01-09 22:21:36 +0700

    add comment scheduler & student

--- NachOS-4.0/code/test/scheduler.c
+++ NachOS-4.0/code/test/scheduler.c
@@ -1,51 +1,95 @@
 #include "syscall.h"
 
+#define MAX_STUDENT 9	// Số lượng sinh viên tối đa
 
 void main()
 {
-	int numStudent, outputFile, contactFile, i, j;
-	char* res;
+	/*Tạo các biến cần thiết:
+		outputFile	:	OpenFileID của file output.txt
+		mailFile	:	OpenFileID của file mailbox.txt
+		numStudent	:	số lượng sinh viên lấy nước
+		id			:	id của sinh viên
+		SIDs		:	SpaceID của các tiến trình con
+		output		:	chuỗi chứa kết quả
+		i, j		:	biến đếm
+	*/
+	int outputFile, mailFile;
+	int numStudent;
 	char id;
-	int IDs[10];
+	int SIDs[10];
+	char* output;
+	int i, j;
 
-	CreateSemaphore("WaterTap", 1);
+
+	/* Tạo các semaphore cần thiết:
+		WaterTap	:	semaphore quản lí việc sử dụng vòi nước,
+						trong một thời điểm chỉ có tối đa một người dùng
+		send		:	semaphore quản lí việc nhận message,
+						chỉ khi nhận được message từ process cha thì process con mới thực thi tiếp
+		send		:	semaphore quản lí việc gửi message,
+						chỉ khi process con nhận được message thì process cha mới thực thi tiếp
+	*/
+	CreateSemaphore("WaterTap", 0);		// Ban đầu vòi nước đóng. Vòi nước sẽ được mở phía bên dưới.
 	CreateSemaphore("send", 0);
 	CreateSemaphore("recv", 0);
-	CreateSemaphore("begin", 0);
+
+	/* Tạo các file cần thiết:
+		output.txt	:	file chứa kết quả
+		mailbox.txt	:	file chứa các message được trao đổi giữa các process,
+						cụ thể ở bài này, chúng em tạo ra file mainbox.txt để process cha 
+						có thể gửi cho các process sinh viên biết về mã sinh viên của nó
+	*/
 	CreateFile("output.txt");
-	CreateFile("contact.txt");
+	CreateFile("mailbox.txt");
 
-	PrintString("Nhap so sinh vien: ");
-	numStudent = ReadNum();
 
+	// Nhập số lượng sinh viên lấy nước, phải là một số từ 1 đến 9, nếu không đúng sẽ yêu cầu nhập lại.
+	do {
+		PrintString("Enter number of student: ");
+		numStudent = ReadNum();
+	} while (numStudent < 1 || MAX_STUDENT < numStudent);
+
+
+	// Tạo các process con để thực thi chương trình student
 	for (i=1; i<=numStudent; i++) {
-		IDs[i] = Exec("student");
+		// Tạo nhánh mới thực thi chương trình student. Lưu lại SpaceID trong mảng SIDs.
+		SIDs[i] = Exec("student");
 
+		// Tạo ID cho process con vừa được phân nhánh
 		id = (char)(i + '0');
-		PrintString(id);
-		contactFile = Open("contact.txt", 0);
-		Write(&id, 1, contactFile);
-		Close(contactFile);
 
-		Signal("send");
-		Wait("recv");
+		// Ghi message chứa ID của process con vào file mailbox.txt
+		mailFile = Open("mailbox.txt", 0);
+		Write(&id, 1, mailFile);
+		Close(mailFile);
+
+		// Đợi process con nhận được message rồi tiếp tục
+		Signal("send");		// Đánh dấu đã gửi
+		Wait("recv");		// Đợi phản hồi
 	}
 
-	PrintString("-------------------------------\n");
-	for (i=1; i<=numStudent; i++) Signal("begin");
 
+	// --- Mở vòi nước và cho các sinh viên lấy nước ---
+	PrintString("-------------------------------\n");
+	// Mở vòi nước.
+	Signal("WaterTap");
+
+	// Đợi các sinh viên lấy nước xong.
 	for (i=1; i<=numStudent; i++) 
-		if (IDs[i] >= 0)
-			Join(IDs[i]);
+		if (SIDs[i] >= 0)
+			Join(SIDs[i]);
 
 	PrintString("-------------------------------\n");
+
+
+	// Kiểm tra file output.txt
+	PrintString("\nThu tu lay nuoc la: \n");
 
 	outputFile = Open("output.txt", 0);
-	PrintString("\nThu tu lay nuoc la: \n");
-	while(Read(res, 1, outputFile) > 0) PrintString(res);
-	PrintChar('\n');
-
+	while(Read(output, 1, outputFile) > 0) PrintString(output);
 	Close(outputFile);
 
+
+	// Kết thúc chương trình
     Exit(0);
 }
--- NachOS-4.0/code/test/student.c
+++ NachOS-4.0/code/test/student.c
@@ -1,44 +1,66 @@
 #include "syscall.h"
 
+#define SWATER 10   // Số lít nước mỗi sinh viên cần lấy
 
 void main()
 {
-    int waitTime, outputFile, contactFile, i, j, k;
+	/* Tạo các biến cần thiết:
+		outputFile	:	OpenFileID của file output.txt
+		mailFile	:	OpenFileID của file mailbox.txt
+		waitTime    :   thời gian lấy nước của sinh viên
+		id			:	id của sinh viên
+        tmp         :   một chuỗi phụ để phụ vụ việc đọc ghi file sẽ dùng bên dưới.
+        i, j, k     :   các biến đếm
+	*/
+    int outputFile, mailFile;
+    int waitTime = RandomNum() % 100000;    // Thời gian lấy nước là một số nguyên dương ngẫu nhiên bé hơn 10.000
     char id;
     char* tmp;
+    int i, j, k;
 
-    Wait("send");
-    contactFile = Open("contact.txt", 0);
-    Read(&id, 1, contactFile);
-	Close(contactFile);
-    Signal("recv");
-    //PrintString("Here");
-    Wait("begin");
 
+    // Quá trình process sinh viên nhận ID từ process cha.
+    Wait("send");                       // Đợi process cha gửi message chứa ID
+    mailFile = Open("mailbox.txt", 0);  // Mở mailbox
+    Read(&id, 1, mailFile);             // Lấy message
+	Close(mailFile);                    // Đóng mailbox
+    Signal("recv");                     // Thông báo nhận thành công để process thực thi tiếp
+
+
+    // --- Tiến hành lấy nước ---
+
+    // Mở output.txt để ghi kết quả
     outputFile = Open("output.txt", 0);
 
-    for (i=0; i<10; i++){
+    // Mỗi sinh viên lấy nước 10 lần, mỗi lần được mô tả trong vòng lặp sau đây
+    for (i=0; i<SWATER; i++){
+        // Đợi vòi nước mở
         Wait("WaterTap");
 
-        waitTime = RandomNum() % 100000;
+        // Lấy nước, vòng for thể hiện thời gian lấy nước của sinh viên
         for (j=0; j<waitTime; j++);
 
-        for (k=1; k<(int)(id-'0'); k++) PrintString("      ");
+        // Ghi vào file kết quả
+        while(Read(tmp, 1, outputFile) > 0);    // Tận dụng lệnh read để di chuyển con trỏ đến cuối file
+        Write(&id, 1, outputFile);              // Ghi ID sinh viên vào file cho biết thứ tự sinh viên này lấy nước
+        tmp = " ";
+        Write(tmp, 1, outputFile);              // GHi dấu khoảng trắng
+
+        // Phần in ra màn hình để theo dõi
+        for (k = 1; k < (int)(id-'0'); k++) PrintString("      ");
         PrintChar(id);
         PrintString("\n");
 
-        while(Read(tmp, 1, outputFile) > 0);
-        tmp = " ";
-        Write(&id, 1, outputFile);
-        Write(tmp, 1, outputFile);
-
+        // Trả lại vòi nước
         Signal("WaterTap");
     }
-    Close(outputFile);
+    Close(outputFile);  // Đóng file kết quả
     
+    // Thông báo lên màn hình đã thực hiện xong
     PrintString("(Student ");
     PrintChar(id);
     PrintString(" done)\n");
 
+    // Kết thúc process
     Exit(0);
 }
